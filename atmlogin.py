9bea49023934a886a5c934ad614a2f36f52ab87e
AkosuaAmpaw <91437975+AkosuaAmpaw@users.noreply.github.com>
2021-10-13 17:40:07 +0000

    Update atmlogin.py

--- atmlogin.py
+++ atmlogin.py
@@ -13,7 +13,14 @@ userArray=[
 {'username':'akosua', 'pin':4000, 'balance':{'GHS':140, 'USD':0} },
 {'username':'eben', 'pin':5461, 'balance':{'GHS':140, 'USD':0} },
 {'username':'bagna', 'pin':7438, 'balance':{'GHS':555000.00, 'USD':300000.00} },
-{]
+{'username':'esinam', 'pin':1110, 'balance':{'GHS':5000.00, 'USD':20000.00} },
+{'username':'dna', 'pin':1234, 'balance':{'GHS':25000.00, 'USD':15000.00} },
+{'username':'aba', 'pin':2121, 'balance':{'GHS':100000.00, 'USD':80000.00} },
+{'username':'bet', 'pin':3197, 'balance':{'GHS':995000.00, 'USD':980000.00} },
+{'username':'van', 'pin':2111, 'balance':{'GHS':15000.00, 'USD':80000.00} },
+{'username':'dave', 'pin':1034, 'balance':{'GHS':250.00, 'USD':8000.00} },
+{'username':'emmanuel', 'pin':1634, 'balance':{'GHS':45000.00, 'USD':680000.00} },
+]
 
 
         
